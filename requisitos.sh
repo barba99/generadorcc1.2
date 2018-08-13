32a222205242bc8ecb20320854f450ce73279f9e
barba99 <38580791+barba99@users.noreply.github.com>
2018-08-13 09:26:44 -0500

    Create requisitos.sh

---
+++ requisitos.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+#herramientas para que funcione al cien  #generadorcc.1.2
+#Descargando requisitos del sistema
+
+apt update && apt upgrade -y
+
+pkg install python2
+pkg install w3m
+pkg install neofetch
